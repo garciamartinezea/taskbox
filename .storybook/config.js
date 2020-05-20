25fc7f5a20bdc72cac2d0d26967146d1a4a219a5
Ernesto <garciamartinez@gmail.com>
2020-05-20 14:42:46 -0500

    init

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,13 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+import 'chromatic';
+
 import '../src/index.css';
 
-const req = require.context('../src', true, /\.stories.js$/);
+const req = requireContext('../src', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
-
