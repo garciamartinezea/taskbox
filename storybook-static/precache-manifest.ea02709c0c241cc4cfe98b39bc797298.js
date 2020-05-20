a651bfad8c7d4a596f121e1c35f007f2d9e903b8
Ernesto <garciamartinez@gmail.com>
2020-05-20 14:43:38 -0500

    init

---
+++ storybook-static/precache-manifest.ea02709c0c241cc4cfe98b39bc797298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24faee406650f4a30e28692f5d1e2e3f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f1b14189df2fc74d65df.bundle.js"
+  },
+  {
+    "url": "runtime~main.f1b14189df2fc74d65df.bundle.js"
+  },
+  {
+    "revision": "9ffabb977bdda2fd6fd6",
+    "url": "static/css/main.f2b987d9.chunk.css"
+  },
+  {
+    "url": "vendors~main.f1b14189df2fc74d65df.bundle.js"
+  },
+  {
+    "url": "vendors~main.f1b14189df2fc74d65df.bundle.js.LICENSE.txt"
+  }
+]);
